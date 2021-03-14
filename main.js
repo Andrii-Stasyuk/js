31460b7e903e54c2d1f036a74972d597025ad04c
Andrii-Stasyuk <andrii.b.stasiuk@gmail.com>
2021-03-14 19:41:40 +0200

    first program

---
+++ main.js
@@ -0,0 +1,12 @@
+const numberOfFilms=prompt("Скільки фільмів?")
+const personalMovieDb = {
+       count: numberOfFilms,
+       movies: {},
+       actors: {},
+       genres: [],
+       privat: false
+}
+a = prompt("sjrfcberfhgc")
+b = prompt("fgfgfgfgfgfg")
+personalMovieDb.movies[a] = b;
+console.log(personalMovieDb);
