c075e30710c4bbc1ee972078fa44aad88f8d625c
Andrii-Stasyuk <andrii.b.stasiuk@gmail.com>
2021-03-19 19:15:43 +0200

    practic2

--- main.js
+++ main.js
@@ -6,7 +6,30 @@ const personalMovieDb = {
        genres: [],
        privat: false
 }
-a = prompt("sjrfcberfhgc")
-b = prompt("fgfgfgfgfgfg")
-personalMovieDb.movies[a] = b;
 console.log(personalMovieDb);
+let i = 0;
+do{
+       const a = prompt("sjrfcberfhgc"),
+       b = prompt("fgfgfgfgfgfg");
+       if(a==="" && b==="" && a.length<50 && b.length<50){
+           console.log("error");
+           i--;
+    }
+           else{
+                  personalMovieDb.movies[a] = b;  
+           }
+           i++;
+    }
+while(i<2);
+  
+ console.log(personalMovieDb);
+
+ if(personalMovieDb.count < 10){
+         console.log("123")
+ }
+ else if(personalMovieDb.count >= 10){
+       console.log("111")
+ }
+ else{
+       console.log("eror")
+ }
